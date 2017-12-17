2174942f592706e12c5ee0ef1eaa38a354ff52ef
Toshimitsu Takahashi <toshi@tilfin.com>
2017-12-17 20:43:09 +0900

    Add serialize_backtrace attr to base formatter

--- lib/ougai/formatters/base.rb
+++ lib/ougai/formatters/base.rb
@@ -3,8 +3,13 @@ require 'socket'
 
 module Ougai
   module Formatters
+    # Base formatter
+    # @attr [Fixnum] trace_indent Specify exception backtrace indent (by default this is 2).
+    # @attr [Fixnum] trace_max_lines Keep exception backtrace lines (by default this is 100).
+    # @attr [Boolean] serialize_backtrace Whether exception should converts String (by default this is on).
     class Base < Logger::Formatter
       attr_accessor :trace_indent, :trace_max_lines
+      attr_accessor :serialize_backtrace
       attr_reader :app_name, :hostname
 
       def initialize(app_name = nil, hostname = nil)
@@ -12,6 +17,7 @@ module Ougai
         @hostname = hostname || Socket.gethostname.force_encoding('UTF-8')
         @trace_indent = 2
         @trace_max_lines = 100
+        @serialize_backtrace = true
         self.datetime_format = nil
       end
 
@@ -25,14 +31,15 @@ module Ougai
           message: ex.to_s
         }
         if ex.backtrace
-          err[:stack] = serialize_trace(ex.backtrace)
+          bt = ex.backtrace.slice(0, @trace_max_lines)
+          err[:stack] = @serialize_backtrace ? serialize_trace(bt) : bt
         end
         err
       end
 
       def serialize_trace(trace)
         sp = "\n" + ' ' * @trace_indent
-        trace.slice(0, @trace_max_lines).join(sp)
+        trace.join(sp)
       end
 
       private
